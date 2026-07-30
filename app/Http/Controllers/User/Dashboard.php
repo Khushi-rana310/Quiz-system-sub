e43dbc9076e47ea7d7d084b7bd48132c9f9e32a5
Khushi Rana <khushithakur8725@gmail.com>
2026-07-30 13:26:49 +0530

    added screenshot folder

--- app/Http/Controllers/User/Dashboard.php
+++ app/Http/Controllers/User/Dashboard.php
@@ -15,9 +15,10 @@ class Dashboard extends Controller
   public function QuizList($id,$cat_name){
      
      if(Session::has('users')){
+            $userdetail = Session::get('users');
             $categories = Catergory::withCount('quizs')->get();
             $get_quizes = QuizModel::where('category_id',$id)->get();
-           return view('user.quiztable',compact('categories','get_quizes','cat_name'));
+           return view('user.quiztable',compact('categories','get_quizes','cat_name','userdetail'));
         }else{
            return redirect('login');   
         }
@@ -25,6 +26,7 @@ class Dashboard extends Controller
   
   
   public function mcq_questions($quizid,$quizname,$cat_name){
+   $userdetail = Session::get('users');
    $categories = Catergory::withCount('quizs')->get();
    $first_mcq_id = McqModal::where('quiz_id',$quizid)->first('id');
    $mcq_detail = McqModal::where('id',$first_mcq_id->id)->first();
@@ -37,18 +39,19 @@ class Dashboard extends Controller
    $currentquiz['quiz_id'] = $quizid;
    $currentquiz['quiz_category'] = $cat_name;
    Session::put('currenQuiz',$currentquiz);
-   return view('user.mcqs',compact('categories','mcq_detail'));
+   return view('user.mcqs',compact('categories','mcq_detail','userdetail'));
   }
 
 
 public function mcq_submit($id){
+ $userdetail = Session::get('users');
  $categories = Catergory::withCount('quizs')->get(); 
  $currentquiz =  Session::get('currenQuiz');
  $currentquiz['currentMCq'] += 1;
  $mcq_detail = McqModal::where([['id', '>', $id],['quiz_id', '=', $currentquiz['quiz_id']]])->first();
  Session::put('currenQuiz',$currentquiz);
 if($mcq_detail){ 
-    return view('user.mcqs',compact('categories','mcq_detail'));
+    return view('user.mcqs',compact('categories','mcq_detail','userdetail'));
 }else{
    return "result page
    ";
--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -36,7 +36,7 @@ class LoginController extends Controller
         // Redirect to dashboard
         $username = $user->name;
         //  return redirect()->route('user.dashboard',compact('username'));
-         return view('user.dashboard',compact('username'));
+         return redirect('dashboard');
     }
 
     // Invalid credentials
@@ -65,6 +65,19 @@ class LoginController extends Controller
         // Redirect to login page with success message
         return redirect()->route('login')->with('success', 'Registration successful. Please log in.');
     }
+    
+    //
+    public function dashboard(){
+
+        if(Session::has('users')){
+            $userdetail = Session::get('users');
+            $categories = Catergory::withCount('quizs')->get();
+            return view('user.dashboard',compact('categories','userdetail'));
+        }else{
+           return redirect('login');  
+        }
+    }
+    
 
     public function logout(){
         if(Session::has('users')){
